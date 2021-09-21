939e421a4f4ad61815da79dff311eac3ce6a7731
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-09-21 15:59:21 +0700

    :fire: building out the about section
    done

--- src/sections/About/index.js
+++ src/sections/About/index.js
@@ -1,7 +1,67 @@
-// This is About component, It will contain about us info.
+import React from "react";
+
+import {
+  AboutContainer,
+  Waves,
+  Hand,
+  MainContent,
+  CurvedLine,
+  Title,
+  ContentContainer,
+  Rocket,
+  Human,
+  Text,
+  Circle,
+  AboutText,
+  SectionTitle,
+} from "./styles/About";
+
+import wave from "../../assets/waves.svg";
+import rocket from "../../assets/rocket image.png";
+import human from "../../assets/human.svg";
+import hand from "../../assets/hand.svg";
 
 const About = () => {
-  return <div>About Component</div>;
+  return (
+    <AboutContainer>
+      <Waves src={wave} alt="surfing waves svg" />
+
+      <Hand>
+        <img src={hand} alt="hand pic" />
+      </Hand>
+
+      <MainContent>
+        <SectionTitle>
+          <Title>About Us</Title>
+          <CurvedLine />
+        </SectionTitle>
+
+        <ContentContainer>
+          <Rocket>
+            <img src={rocket} alt="rocket animation" width="400" height="400" />
+          </Rocket>
+
+          <AboutText>
+            <Human>
+              <img src={human} alt="" width="400" height="400" />
+            </Human>
+
+            <Text>
+              We help our customers to tell about themselves, to grow and stand
+              out in an increasingly competitive digital world, through creative
+              projects that are able to attract and involve, creating strategic
+              value.
+            </Text>
+            <div>
+              <Circle style={{ backgroundColor: "var(--purple)" }} />
+              <Circle style={{ backgroundColor: "var(--pink)" }} />
+              <Circle style={{ backgroundColor: "var(--black)" }} />
+            </div>
+          </AboutText>
+        </ContentContainer>
+      </MainContent>
+    </AboutContainer>
+  );
 };
 
 export default About;
---
+++ src/sections/About/styles/About.js
@@ -0,0 +1,134 @@
+import styled, { keyframes } from "styled-components";
+
+export const move = keyframes`
+    0% { transform: translateY(-5px)         }
+    50% { transform: translateY(10px) translateX(10px)        }
+    100% { transform: translateY(-5px)         }
+`;
+
+export const AboutContainer = styled.section`
+  width: 100vw;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Waves = styled.img`
+  width: 100%;
+  height: auto;
+  position: absolute;
+  top: -1rem;
+`;
+
+export const Hand = styled.div`
+  position: absolute;
+  bottom: -1rem;
+  right: 0;
+  @media only Screen and (max-width: 40em) {
+    display: none;
+  }
+`;
+
+export const MainContent = styled.div`
+  margin: 0 15rem;
+  margin-top: 15rem;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+
+  @media only Screen and (max-width: 64em) {
+    margin: 0 calc(5rem + 5vw);
+    margin-top: 10rem;
+  }
+  @media only Screen and (max-width: 40em) {
+    align-items: center;
+    margin: 3rem calc(3rem + 3vw);
+  }
+`;
+
+export const SectionTitle = styled.div`
+  @media (min-width: 1800px) {
+    margin-top: 100px;
+
+    transform: translateY(20px);
+  }
+  @media (min-width: 1100px) {
+    transform: translateY(30px);
+  }
+`;
+
+export const Title = styled.h1`
+  font-size: 2rem;
+  display: inline-block;
+`;
+
+export const CurvedLine = styled.div`
+  width: 7rem;
+  height: 2rem;
+  border: solid 5px var(--purple);
+  border-color: var(--purple) transparent transparent transparent;
+  border-radius: 150%/60px 70px 0 0;
+`;
+
+export const ContentContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  @media (min-width: 1100px) {
+    margin-top: 30px;
+  }
+
+  @media only Screen and (max-width: 40em) {
+    flex-direction: column;
+  }
+`;
+
+export const Rocket = styled.div`
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  width: 40%;
+  padding-bottom: 5rem;
+  animation: ${move} 2.5s ease infinite;
+  @media only Screen and (max-width: 40em) {
+    width: 50vw;
+    padding-bottom: 0;
+  }
+`;
+
+export const Human = styled.div`
+  width: 50%;
+  position: absolute;
+  right: 0;
+  bottom: 100%;
+  @media only Screen and (max-width: 40em) {
+    display: none;
+  }
+`;
+
+export const Text = styled.h4`
+  font-size: calc(0.5rem + 1vw);
+  line-height: 1.5;
+  color: var(--nav2);
+`;
+
+export const Circle = styled.span`
+  display: inline-block;
+  width: 1rem;
+  height: 1rem;
+  border-radius: 50%;
+  background-color: black;
+  margin-right: 0.5rem;
+  margin-top: 1rem;
+`;
+
+export const AboutText = styled.div`
+  width: 50%;
+  position: relative;
+  @media only Screen and (max-width: 40em) {
+    width: 100%;
+  }
+`;
